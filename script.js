12c98873faa93db4df0e4358dd421c4422a78bed
giack-dev <github.giack@gmail.com>
2023-02-13 17:44:39 +0100

    removed all bootstrap

--- script.js
+++
@@ -1,7 +0,0 @@
-const myCarousel = document.querySelector(".carousel-inner");
-
-const carousel = new bootstrap.Carousel(myCarousel, {
-  cycle: true,
-  interval: 5000,
-  touch: false,
-});
---
+++ scripts/translate.js
@@ -0,0 +1,10 @@
+//Translate Section
+
+const data = {
+  italian: {
+    title: "Archivio Terremoti",
+  },
+  english: {
+    title: "Earthquake Archive",
+  },
+};
