aabe08588ea061a322b2f9154a76c1df905c6c01
Lorenzo <dichiaralorenzo4@gmail.com>
2023-02-13 10:53:35 +0100

    change name

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,7 @@ class NavBar extends HTMLElement {
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
         <img src="./assets/logo.png" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Archivio Terremoti</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
