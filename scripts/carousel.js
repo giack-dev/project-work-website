be2ea78fc054fb324fe3dea2006cad5ef39e2874
Lorenzo <dichiaralorenzo4@gmail.com>
2023-02-14 09:47:39 +0100

    changes

---
+++ scripts/carousel.js
@@ -0,0 +1,19 @@
+const img = document.getElementById("img");
+let i = 0;
+
+function Avanti() {
+  i++;
+  if (i > 3) {
+    i = 1;
+  }
+  img.src = "./assets/Terremoti_Mappa" + i + ".jpg";
+}
+
+function Indietro() {
+  i--;
+
+  if (i < 1) {
+    i = 3;
+  }
+  img.src = "./assets/Terremoti_Mappa" + i + ".jpg";
+}
